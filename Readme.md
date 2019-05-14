81053370dbb28353d382ca47f119a1d67dac34a2
xiongxiaopeng <xiongxiaopeng@ehomepay.com.cn>
2019-05-14 18:31:27 +0800

    refresh roadmap

--- Readme.md
+++ Readme.md
@@ -29,17 +29,23 @@
   
 ## RoadMap
 ### version 0.5.0
-* 具有访问日志
-* 支持iphash 负载策略
-* 支持基于tag切换的负载策略
-* 支持access的黑白名单
+* 具有访问日志 
+* 支持iphash 负载策略 --ok
+* 支持基于tag切换的负载策略 --ok
+* 支持access的黑白名单 --ok
+
+### version 0.6.0
+* 支持从配置文件中注册api、server接口
+* 支持静态资源
 
 
 ### version 0.8.0
-* 支持accesstoken方式的鉴权
-* 实现动态注册api、server接口
-* 支持网关集群
+* 支持accesstoken方式的鉴权（存储走redis)
+* 支持权限配置
+* 支持reload方法来重新装载权限配置
+
 
 ### version 1.0.0
-* 支持spring cloud的注册中心服务发现
+* 实现动态注册api、server接口、动态权限接口
+* 支持网关集群
 * 支持熔断
--- runtime/utils.go
+++ runtime/utils.go
@@ -1,5 +1,12 @@
 package runtime
 
+import (
+	"crypto/md5"
+	"fmt"
+	"strconv"
+	"time"
+)
+
 func min(a, b int) int {
 	if a <= b {
 		return a
@@ -17,4 +24,34 @@ func int2BytesTo(v int, ret []byte) {
 
 func byte2Int(data []byte) int {
 	return int((int(data[0])&0xff)<<24 | (int(data[1])&0xff)<<16 | (int(data[2])&0xff)<<8 | (int(data[3]) & 0xff))
+}
+
+const (
+	timeLayout = "2006-01-02 15:04:05"
+)
+
+func GetNowDate() string {
+	return time.Now().Format(timeLayout)
+}
+
+func ToDate(t time.Time) string {
+	return t.Format(timeLayout)
+}
+
+//字符生成md5
+func GetMd5str(value string) string {
+	data := []byte(value)
+	has := md5.Sum(data)
+	md5str1 := fmt.Sprintf("%x", has) //将[]byte转成16进制
+
+	return md5str1
+
+}
+
+func ToInt(value string) int64 {
+	result, err := strconv.Atoi(value)
+	if err == nil {
+		return int64(result)
+	}
+	return 0
 }
