b731e7812ba6052b13fb4c962fa763152155509a
xiongxiaopeng <xiongxiaopeng@ehomepay.com.cn>
2019-06-18 10:00:30 +0800

    complete token handle process

--- extends/accesstoken.go
+++ extends/accesstoken.go
@@ -25,24 +25,44 @@ func (this *AccessTokenImp) Add(tb TokenCreator) {
 	}
 }
 func (this *AccessTokenImp) Call(req *fasthttp.Request) *fasthttp.Response {
-	fmt.Println(req.PostArgs())
-	fmt.Println(string(req.RequestURI()))
 	res := fasthttp.Response{}
+
+	args := req.URI().QueryArgs()
+
 	//读取参数
 	var grandType, user, secret string
-	res.SetBodyString("hello!")
-
-	if len(this.builder) > 0 {
-		for _, tb := range this.builder {
-			if tb.GetGrandType() == grandType {
-				b, token := tb.BuildToken(user, secret)
-				if b {
-					fmt.Println(token)
-					res.SetBodyString(fmt.Sprintf(`{access_token:"%s"}`, token))
-				}
+	grandType = string(args.Peek("grandtype"))
+	user = string(args.Peek("user"))
+	secret = string(args.Peek("secret"))
+	if grandType == "" || user == "" {
+		res.SetBodyString("the parameter grandtype and user is must!")
+	} else {
+		tb := this.getTokenCreator(grandType)
+		if tb == nil {
+			res.SetBodyString("the grandtype not surport!")
+		} else {
+			b, token := tb.BuildToken(user, secret)
+			if b {
+				fmt.Println(token)
+				res.SetBodyString(fmt.Sprintf(`{access_token:"%s"}`, token))
+			} else {
+				res.SetBodyString("access deny!")
 			}
 		}
 	}
 
 	return &res
+}
+
+func (this *AccessTokenImp) getTokenCreator(t string) TokenCreator {
+	if len(this.builder) > 0 {
+		for _, tb := range this.builder {
+			if tb.GetGrandType() == t {
+
+				return tb
+			}
+		}
+	}
+
+	return nil
 }
